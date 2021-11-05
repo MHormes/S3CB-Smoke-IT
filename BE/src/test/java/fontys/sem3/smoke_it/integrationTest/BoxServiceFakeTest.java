cb8cb12201716778702da545e7af336de95d9be9
Maarten Hormes <I456806@fhict.nl>
2021-11-05 18:31:16 +0100

    had to add @transactional ann. in box test

--- BE/src/test/java/fontys/sem3/smoke_it/integrationTest/BoxServiceFakeTest.java
+++ BE/src/test/java/fontys/sem3/smoke_it/integrationTest/BoxServiceFakeTest.java
@@ -7,11 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Transactional
 @ActiveProfiles("test")
 @SpringBootTest
 class BoxServiceFakeTest {
