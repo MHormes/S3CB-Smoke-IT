eea9b0dc812ff6e6fc3176af2a33e51ea5d08a3b
Maarten Hormes <I456806@fhict.nl>
2021-12-17 13:58:24 +0100

    updated files for submission

--- FE/app-frontend/cypress/integration/smoke-it/LoginTest.js
+++ FE/app-frontend/cypress/integration/smoke-it/LoginTest.js
@@ -1,13 +1,1 @@
-describe('My First Test', () => {
-    it('find the content "type"', () => {
-      cy.visit("https://example.cypress.io")
-
-      cy.contains('type').click()
-
-      cy.url().should('include', '/commands/actions')
-
-      cy.get('.action-email')
-      .type("mymail@gmail.com")
-      .should('have.value', "mymail@gmail.com")
-    })
-  })
+//THIS WILL COME IN THE FUTURE
